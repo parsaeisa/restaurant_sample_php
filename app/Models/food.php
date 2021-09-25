2e60826143971bfdec80abaf560ded8829434d9f
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-09-26 00:33:41 +0330

    feature(menu) : try to show related data

--- app/Models/food.php
+++ app/Models/food.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection ;
 
 class food extends Model
 {
@@ -11,6 +12,11 @@ class food extends Model
 
     public function ingredients()
     {
-        return $this->belongsToMany(Ingredient::class);
+        return $this->belongsToMany(Ingredient::class,
+            'food_ingredients',
+            'food_ingredients',
+            'food_id',
+            'ingredients_id')
+            ->withPivot('food_ingredients');
     }
 }
